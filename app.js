2f1c5c5017d857d6f3e9b990154a91dbabdde458
Stephen Hess <stephen.k.hess@gmail.com>
2018-02-14 16:52:49 -0500

    added /sources endpoint

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ module.exports = express()
   }))
   .use(morgan('combined'))
   .use('/sample', require('./sample'))
+  // The sources router must always be exposed on the /sources endpoint since
+  // it requests files from github.com/openaddresses/openaddresses from the sources/
+  // directory.  Both of the following are needed for complete operation.
+  .use('/sources/*', require('./sources'))
+  .use('/sources', require('./sources'))
   .use('/submit', require('./submit'))
   .use('/upload', require('./upload'))
   .use(express.static(__dirname + '/public'));
